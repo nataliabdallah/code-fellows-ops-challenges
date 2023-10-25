f4e4e813022b3121a2260a1b77b46ac9ecd3c5da
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-10-25 13:38:58 -0700

    attempting delete line 35

--- Challenges/logins.sh
+++ Challenges/logins.sh
@@ -7,7 +7,6 @@
 
 # Declaration of variables
 
-# PRICE OF APPLES
 # One equal sign means assigining a value
 # Two is comparing values
 LOGINS=`last -a`
@@ -32,6 +31,39 @@ print_greeting
 
 print_greeting
 
-echo $greeting
+
+# End
+
+#!/bin/bash
+
+# Script Name:                  Functions
+# Author Name:                  Nathalie Abdallah
+# Date of latest revision:      10/25/2023
+# Purpose:                      Print simple Functions
+
+# Declaration of variables
+
+# One equal sign means assigining a value
+# Two is comparing values
+
+WHO=`whoami`
+
+# Declaration of functions
+# Basic function that handles printing the greeting to the screen
+print_greeting () {
+    echo "HELLO WORLD"
+    echo "This is our first function"
+    echo "The person running this script is $WHO"
+}
+
+
+# Main
+
+# CALL THE FUNCTION - TELL THE COMPUTER TO DO THE THING
+print_greeting
+
+print_greeting
+
+print_greeting
 
 # End
