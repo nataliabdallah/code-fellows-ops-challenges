5ac7c27b2c2eff6a8bfb0406dc5a855de6a9975a
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-10-28 17:08:11 -0700

    editted script

--- Challenges/5_201_loops.sh
+++ Challenges/5_201_loops.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-# Script Name:                  OPS 201 Challenge 5 LOOPS
-# Author Name:                  Nathalie Abdallah
-# Date of latest revision:      10/27/2023
-# Purpose:                      Write 
+# Script:                     Loop 
+# Author:                     Nathalie Abdallah
+# Date of latest revision:    10/27/2024  
+# Purpose:                    Ops 201 Challenge 05
 
-# Objective
-
-# Write 
-
-# Task
-
-# Diplays the runing processes
+# Tasks
+# Write a script that:
+# Displays running processes
 # Asks the user for a PID
 # Kills the process with that PID
-# Starts over 
+# Starts over at step 1 and continues until the user exits with Ctrl + C
+# Use a loop so that the script will continuously start over, displaying the running processes, asking the user for input, etc.
+# For this assignment, initialize a process that won't harm the OS's functionality upon termination. Don't kill essential processes required for the OS to work, such as kernel drivers.
+# Hint: you can open a second terminal in your development environment and start it pinging one of your other machines. This will be a safe process to target.
 
-# Define an array
-names=("Bob" "Jane" "Mike" "Sarah")
-COUNT=4
-# FOR LOOP
-# Basic syntax
-for name in ${names[@]}
-do
-    echo $name
-done
 
 # WHILE LOOP
-# Basic suntax
-while [ $COUNT -gt 0 ]; do
-    echo "Value of count is: $COUNT"
-    COUNT=$(($COUNT -1)) # this is the line if you remove, it will create an infinite loop (hit cntrl-c to kill the loop)
+while true; do
+  echo "Running processes:"  # display running processes
+  ps auxf 
+  read -p "Enter PID to kill (Ctrl + C to exit): " pid # ask user for PID
+
+  # check if input is a valid PID
+  if [ $pid =~ ^[0-9]+$ ]] ]; then
+    echo "entered invalid PID, try again."
+  else
+    # check if PID is valid
+    if ps -p "$pid" > /dev/null; then
+      #  kill process
+      kill "$pid"
+      echo "Killed process with PID: $pid"
+    else
+      echo "No mathing process to $pid."
+    fi
+fi
 done
-# While (condition) is true, do the actions in the while loop, until that condition is not true. Infinite Loops: error when you don't implement that counter- to break out of the loop
-
-echo "The condition is no longer true, we broke out of the while loop!"
-
-
-# Main
