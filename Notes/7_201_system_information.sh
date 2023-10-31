aebe6f0e6a79e9d627b8562a6d8ab0554c77e2b9
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-10-31 09:02:28 -0700

    created notes challenge 7

---
+++ Notes/7_201_system_information.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+#Script Name:               System Information
+# Author:                   Nathalie Abdallah
+# Date of latest revision:  10/30/2023
+# Purpose:                  Create a script that...
+
+# Uses lshw to display system information to the screen about the following components:
+# Name of the computer
+# CPU
+# Product
+# Vendor
+# Physical ID
+# Bus info
+# Width
+# RAM
+# Description
+# Physical ID
+# Size
+# Display adapter
+# Description
+# Product
+# Vendor
+# Physical ID
+# Bus info
+# Width
+# Clock
+# Capabilities
+# Configuration
+# Resources
+# Network adapter
+# Description
+# Product
+# Vendor
+# Physical ID
+# Bus info
+# Logical name
+# Version
+# Serial
+# Size
+# Capacity
+# Width
+# Clock
+# Capabilities
+# Configuration
+# Resources
+# Uses grep to remove irrelevant information from the lshw output
+# Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
+# Runs as Root; you may execute the shell script with sudo or as Root
+
+
