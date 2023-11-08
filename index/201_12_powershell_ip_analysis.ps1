3e583068f7007e60e9956d99508557120029ccd4
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-11-08 14:02:37 -0800

    editted Challenge 12-fixed order

--- index/201_12_powershell_ip_analysis.ps1
+++ index/201_12_powershell_ip_analysis.ps1
@@ -7,21 +7,29 @@
 :: Use Select-String to search network_report.txt and return only the IP version 4 address.
 :: Remove the network_report.txt when you are finished searching it.
 
-:: Variables so I don't have to write out this entire path and name of file
+# Variables so I don't have to write out this entire path and name of file
 
-$target = network_report.txt
-$path = C:\Users\natali\Documents\
-$create_report = ipconfig /all > $path$target  :: This creates the file and inputs the info ipconfig all
-$Pluck_IP4 = Select-String -Path $path$target -Pattern 'IPv4' :: once the file is created, this string that will print out on powershell will be anything where this keyword is used IPv4
-$Erase = rm $path$target; echo "Report erased"
 
-:: Functions that will create the file with IPCONFIG all data, then pluck out IPv4 from the file, and display it onto Powershel, then delete the file that was created.
-Function Get-IPV4 {
+$create_report = ipconfig /all > C:\Users\natali\Documents\network_report.txt  :: This creates the file and inputs the info ipconfig all
+$Pluck_IP4 = Select-String -Path C:\Users\natali\Documents\network_report.txt -Pattern 'IPv4' :: once the file is created, this string that will print out on powershell will be anything where this keyword is used IPv4
+$Erase = rm C:\Users\natali\Documents\network_report.txt; echo "Report erased"
+
+# Functions that will create the file with IPCONFIG all data, then pluck out IPv4 from the file, and display it onto Powershel, then delete the file that was created.
+Function Create-NetworkReport {
     $create_report
-    $Pluck_IP4
+}
+
+Function Get-IPV4 {
+    Write-Output $Pluck_IP4
+   
+}
+
+Function Erase-Report {
     $Erase
 }
 
-:: Main
+# Main
 
+Create-NetworkReport
 Get-IPv4
+Erase-Report
