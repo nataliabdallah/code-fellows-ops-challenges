0d5815951e00f408290c820e318ba3be541fd739
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-11-09 11:14:10 -0800

    created Challenge 14

---
+++ index/201_14_github.sh
@@ -0,0 +1,24 @@
+### Welcome to my Code Fellows Cybersecurity Repository, focusing on mastering Bash and PowerShell scripting for OPS 201!
+In this Repository, you'll discover a wealth of knowledge gained during OPS 201, where I learned to automate tasks, bolster security measures, and enhance operational efficiency. The fundamentals concepts covered, including command-line basics, scripting fundamentals, security automation, and log analysis, provide a strong foundation for my scripting skills.
+<br>
+<br>
+These shell scripting examples can be directly applied to the workplace, automating routine security tasks and improving an organization's security posture. With more time, the purpose is to refine and expand these scripts and deepen expertise in Bash and PowerShell scripting, ultimately strengthening one of the many cybersecurity skillset.
+
+# ops-201d10-code-challenges
+Code Challenges
+<br>
+| [2](index/201_2_my_first_bash_script.sh) | [3](index/201_3_functions.sh) | [4](index/201_4_arrays.sh) | [5](index/201_5_loops.sh) |
+|:----------------------:|:-----------------------:|:----------------------:|:----------------------:|
+| [6](index/201_6_conditionals.sh) | [7](index/201_7_system_information.sh) | [8](index/201_8_windows_batch_scripting.bat) | [9](index/201_9_log_retrieval_via_powershell.ps1) 
+| [10](index/201_10_system_process_commands.ps1) | [11](index/201_11_automated_endpoint_configuration.ps1) | [12]() | [13](index/201_13_domain_analyzer.sh) 
+| [14](index/201_14_github.sh) | [15]() | [16]() | [17]()
+
+<br>
+
+<br>
+
+
+
+Feel free to navigate through my repos to see my Challenges 🍓
+
+
