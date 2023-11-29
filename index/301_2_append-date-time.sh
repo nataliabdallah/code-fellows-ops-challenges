8bd9f73f17bbae21cabbc0c77368543f154446d2
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2023-11-28 17:44:55 -0800

    301_2_append-time-date

---
+++ index/301_2_append-date-time.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Append Date Time
+# Author Name:                  Nathalie Abdallah
+# Date of latest revision:      11/27/2023
+# Purpose:                      Create a bash script that:
+
+#Copies /var/log/syslog to the current working directory
+#Appends the current date and time to the filename
+
+# variables
+copy="/var/log/syslog"
+timestamp=$(date +"%Y-%m-%d-%H:%M:%S")
+file="/home/$USER/Desktop/syslog_$timestamp"  # corrected file path and added missing double quote
+
+# copy syslog file to the current working directory with timestamp
+cp "$copy" "$file"  # added double quotes to handle paths with spaces
+
+echo "command executed successfully!"
+
