cd9295f0d030300174829027d4f79a07eb79b00b
nathalieabdallah <nathalie.e.abdallah@gmail.com>
2024-01-22 22:19:24 -0800

    turned in something

--- index/401_11_challenge.py
+++ index/401_11_challenge.py
@@ -14,3 +14,27 @@
 # If flag 0x14 received, notify user the port is closed.
 # If no flag is received, notify the user the port is filtered and silently dropped.
 
+import scapy
+from scapy.all import IP, TCP, sr1, send
+
+# Define target and range of ports to scan
+scan_target = "scanme.nmap.org"
+target_ports = range(22, 444)  
+local_port = 1027
+
+
+
+# Scanning each port
+for port in target_ports:
+    # Send SYN and wait for response
+    scan_response = sr1(IP(dst=scan_target)/TCP(sport=local_port, dport=port, flags="S"), timeout=1, verbose=0)
+
+    # Analyze response
+    if scan_response is not None and scan_response.haslayer(TCP):
+        if scan_response.getlayer(TCP).flags == 0x12:  # Open port (SYN-ACK)
+            print(f"Port {port} is open!")
+            send(IP(dst=scan_target)/TCP(sport=local_port, dport=port, flags="R"), verbose=0)
+        elif scan_response.getlayer(TCP).flags == 0x14:  # Closed port (RST-ACK)
+            print(f"Port {port} is closed.")
+    else:  # No response or filtered port
+        print(f"Port {port} is filtered or unresponsive.")
