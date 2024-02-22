4d63615beda81bb9a9e97b0dcd59cc0139c70a8b
Nathalie Abdallah <nathalie.e.abdallah@gmail.com>
2024-02-22 08:15:11 -0600

    Update 401_33_detect_part3.py

--- index/401_33_detect_part3.py
+++ index/401_33_detect_part3.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 
+# Script Name:                  Challenge 33 Event Logging part 3/3
+# Author Name:                  Nathalie Abdallah
+# Date of latest revision:      02/21/2023
+# Sources:                      https://chat.openai.com/, https://github.com/codefellows/seattle-ops-301d10
+# Purpose:                      Objectives
+
+import os
+import hashlib
+from datetime import datetime
+from virustotal_python import Virustotal
+from pprint import pprint
+import time
+
+# Initialize the VirusTotal API client
+VIRUSTOTAL_API_KEY = 'api_key_here'
+vtotal = Virustotal(API_KEY=VIRUSTOTAL_API_KEY)
